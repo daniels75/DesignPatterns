427603ea94a2cfba67ced845c30df8b39f83f85b
Daniels <daniel.sadowski75@gmail.com>
2019-07-08 20:47:41 +0200

    Added template for futher design patterns

---
+++ template/README.md
@@ -0,0 +1,10 @@
+---
+title: simpleTitle
+categories: category
+tags:
+ - Java
+ - Gang Of Four
+ - Difficulty-Intermediate
+---
+
+## Template for design pattern
---
+++ template/src/main/java/com/javasolutions/template/simple/AppMain.java
@@ -0,0 +1,12 @@
+package com.javasolutions.pattern.simple;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AppMain {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppMain.class);
+
+    public static void main(String []args) {
+
+    }
+}
